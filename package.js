345015fb1434d70b239f450d0ac60a0e53494c2c
Max Ferguson <maxkferg@gmail.com>
2015-10-18 14:04:02 -0700

    Fix for Meteor 1.2

--- package.js
+++ package.js
@@ -8,22 +8,22 @@ Package.describe({
 Package.onUse(function(api) {
   api.versionsFrom('1.2');
   // Css files from theme
-  api.addAssets("assets/stylesheets/bootstrap.css","client");
-  api.addAssets("assets/stylesheets/responsive.css","client");
-  api.addAssets("assets/stylesheets/font-awesome-all.css","client");
-  api.addAssets("assets/stylesheets/fancybox.css","client");
-  api.addAssets("assets/stylesheets/theme.css","client");
-  api.addAssets("assets/stylesheets/fonts.css","client");
+  api.addFiles("assets/stylesheets/bootstrap.css","client");
+  api.addFiles("assets/stylesheets/responsive.css","client");
+  api.addFiles("assets/stylesheets/font-awesome-all.css","client");
+  api.addFiles("assets/stylesheets/fancybox.css","client");
+  api.addFiles("assets/stylesheets/theme.css","client");
+  api.addFiles("assets/stylesheets/fonts.css","client");
   // JS files from theme
   api.use('jquery');
   api.use('iron:router');
-  api.addAssets("assets/javascripts/bootstrap.js","client");
-  api.addAssets("assets/javascripts/jquery.waitUntilExists.js","client");
-  api.addAssets("assets/javascripts/jquery.flexslider-min.js","client");
-  api.addAssets("assets/javascripts/jquery.tweet.js","client");
-  api.addAssets("assets/javascripts/jquery.fancybox.pack.js","client");
-  api.addAssets("assets/javascripts/jquery.fancybox-media.js","client");
-  api.addAssets("assets/javascripts/script.js","client");
+  api.addFiles("assets/javascripts/bootstrap.js","client");
+  api.addFiles("assets/javascripts/jquery.waitUntilExists.js","client");
+  api.addFiles("assets/javascripts/jquery.flexslider-min.js","client");
+  api.addFiles("assets/javascripts/jquery.tweet.js","client");
+  api.addFiles("assets/javascripts/jquery.fancybox.pack.js","client");
+  api.addFiles("assets/javascripts/jquery.fancybox-media.js","client");
+  api.addFiles("assets/javascripts/script.js","client");
 
 
   // Make these assets available to the client
