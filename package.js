d3b81ec0f129fff3a0379afd54b40f80c77c6a7e
Max Ferguson <maxkferg@gmail.com>
2015-10-18 12:35:12 -0700

    Update for Meteor 1.2

--- package.js
+++ package.js
@@ -1,29 +1,29 @@
 Package.describe({
   name: 'smartbox-theme',
-  summary: 'Smartbox theme for meteor',
-  version: '1.0.0',
+  summary: 'Smartbox theme for Meteor',
+  version: '1.0.1',
   git: 'https://github.com/NitroLabs/meteor-smartbox-theme.git'
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom('1.0');
+  api.versionsFrom('1.2');
   // Css files from theme
-  api.addFiles("assets/stylesheets/bootstrap.css","client");
-  api.addFiles("assets/stylesheets/responsive.css","client");
-  api.addFiles("assets/stylesheets/font-awesome-all.css","client");
-  api.addFiles("assets/stylesheets/fancybox.css","client");
-  api.addFiles("assets/stylesheets/theme.css","client");
-  api.addFiles("assets/stylesheets/fonts.css","client");
+  api.addAssets("assets/stylesheets/bootstrap.css","client");
+  api.addAssets("assets/stylesheets/responsive.css","client");
+  api.addAssets("assets/stylesheets/font-awesome-all.css","client");
+  api.addAssets("assets/stylesheets/fancybox.css","client");
+  api.addAssets("assets/stylesheets/theme.css","client");
+  api.addAssets("assets/stylesheets/fonts.css","client");
   // JS files from theme
   api.use('jquery');
   api.use('iron:router');
-  api.addFiles("assets/javascripts/bootstrap.js","client");
-  api.addFiles("assets/javascripts/jquery.waitUntilExists.js","client");
-  api.addFiles("assets/javascripts/jquery.flexslider-min.js","client");
-  api.addFiles("assets/javascripts/jquery.tweet.js","client");
-  api.addFiles("assets/javascripts/jquery.fancybox.pack.js","client");
-  api.addFiles("assets/javascripts/jquery.fancybox-media.js","client");
-  api.addFiles("assets/javascripts/script.js","client");
+  api.addAssets("assets/javascripts/bootstrap.js","client");
+  api.addAssets("assets/javascripts/jquery.waitUntilExists.js","client");
+  api.addAssets("assets/javascripts/jquery.flexslider-min.js","client");
+  api.addAssets("assets/javascripts/jquery.tweet.js","client");
+  api.addAssets("assets/javascripts/jquery.fancybox.pack.js","client");
+  api.addAssets("assets/javascripts/jquery.fancybox-media.js","client");
+  api.addAssets("assets/javascripts/script.js","client");
 
 
   // Make these assets available to the client
@@ -37,6 +37,6 @@ Package.onUse(function(api) {
     "assets/stylesheets/fonts/fontawesome-webfont.ttf",
     "assets/stylesheets/fonts/corp/fontawesome-corp-webfont.ttf"
   ]
-  api.addFiles(assets,"client");
+  api.addAssets(assets,"client");
 });
 
